643f89931856943eccc0ca01be449a5901a1654a
Harry Tran <Harry Tran>
2019-08-07 21:52:28 +1000

    fix bugs in saving csv files

--- create_reddit_instance.py
+++ create_reddit_instance.py
@@ -4,6 +4,7 @@ import numpy as np
 import datetime
 import os 
 import glob
+import io
 from os.path import join, dirname
 from dotenv import load_dotenv
  
@@ -14,19 +15,17 @@ load_dotenv(dotenv_path)
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 user_agent = os.getenv('USER_AGENT')
-reddit = os.getenv('REDDIT')
-csv_file = os.getenv('CSV_FILE')
 
-# Create input and output paths to read files 
 folder = 'resources'
+url_folder = '/csv_files'
 path = os.path.join(os.getcwd(), folder)
-if not os.path.exists(path+reddit):
-	os.makedirs(path+reddit)
-elif not os.path.exists(path+csv_file):
-	os.makedirs(path+csv_file)
-input_path = path + reddit
-output_path = path + csv_file 
+if not os.path.exists(path+url_folder):
+	os.makedirs(path+url_folder)
+input_path = path + '/reddits'
+output_path = path + url_folder
 today = datetime.date.today()
+print(input_path)
+print(output_path)
 
 # Crawl the data from Reddit 
 def process_url(input_path,output_path):
@@ -35,7 +34,8 @@ def process_url(input_path,output_path):
 			file_path = os.path.join(input_path, filename)
 			f = open(file_path,"r")
 			for text in f:
-				text = text.rstrip('\n')
+				text = text.rstrip('\n').strip()
+				print(text)
 				reddit = praw.Reddit(client_id=client_id,client_secret=client_secret,user_agent=user_agent)
 				# Save the crawled data to CSV file
 				posts = []
@@ -44,13 +44,9 @@ def process_url(input_path,output_path):
 					posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
 				posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
 				posts = posts[posts['body'] != '']
-				if not os.path.exists(output_path+"/"+text):
-					os.makedirs(output_path+"/"+text)
-					posts.to_csv(output_path+"/"+text+"/"+str(text)+"-"+str(today)+".csv")
-			f.close()
+				os.makedirs(output_path+"/"+text)
+				posts.to_csv(output_path+"/"+text+"/"+str(text)+"-"+str(today)+".csv")
 		
-		if not os.path.isfile(file_path):
-			print('Failed to process {}'.format(file_path))
 
 process_url(input_path,output_path)
 print("Crawling Completed");
